9629cfc80171d8c714f3fbe187529f433cc59c05
Tohir <TOXIR_DEV>
2022-08-21 22:22:08 +0600

    in vuex

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
-<script setup lang="ts">
-import { ref } from 'vue';
+<script lang="ts">
+import { computed, ref, defineComponent } from 'vue';
+import "./styles/index.scss";
+import Button from './components/Button.vue';
+import store, { key } from './store';
+import { useStore, mapState, mapGetters, mapMutations } from 'vuex';
 
-interface Todo {
-  list: string,
-}
 
-const todos = ref<Todo[]>([]);
-const list = ref<string>('');
+export default defineComponent({
+  name: "App",
+  components: {
+    Button
+  },
+  setup() {
+    const store = useStore(key);
+    const list = ref<string>('');
 
-function addList(): void {
-  todos.value.push({ list: list.value });
-  list.value = '';
-}
+    const addList = computed(function () {
+      store.commit('additem', {
+        payload: list.value
+      });
+      list.value = '';
+    });
+
+    return {
+      list,
+      addList,
+    }
+  },
+  methods: {
+    ...mapMutations(['additem']),
+  },
+  computed: {
+    ...mapState({
+      todos() {
+        return store.state.todos;
+      }
+    }),
+    ...mapGetters(['todosLenght']),
+  }
+});
 </script>
 
 <template>
@@ -19,15 +46,16 @@ function addList(): void {
     <div class="row justify-content-around">
       <div class="card w-75 p-0">
         <div class="card-header">
+          <h1 class="card-title text-center">{{ todosLenght }}</h1>
           <input type="text" class="form-control" v-model="list">
         </div>
         <div class="card-body d-flex justify-content-center pt-2 pb-2">
-          <button class="btn btn-primary" @click="addList">Add</button>
+          <Button :length="list.length" @addTodoList="() => addList" />
         </div>
         <div class="card-footer" v-if="todos.length > 0">
           <div class="row">
-            <div class="col-12 border border-0 border-bottom mt-1 mb-1 pt-1 pb-1" v-for="{ list } in todos">
-              <p class="card--text m-0">{{ list }}</p>
+            <div class="col-12 border border-0 border-bottom mt-1 mb-1 pt-1 pb-1" v-for="{ item } in todos">
+              <p class="card--text m-0">{{ item }}</p>
             </div>
           </div>
         </div>
@@ -35,9 +63,3 @@ function addList(): void {
     </div>
   </div>
 </template>
-
-<style lang="scss">
-.col-3 {
-  height: 50px;
-}
-</style>
