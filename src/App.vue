4d2e28253182e477f601686162e1dfeb1e4955a2
Tohir <TOXIR_DEV>
2022-08-22 21:50:52 +0600

    ended

--- src/App.vue
+++ src/App.vue
@@ -3,32 +3,28 @@ import { computed, ref, defineComponent } from 'vue';
 import "./styles/index.scss";
 import Button from './components/Button.vue';
 import store, { key } from './store';
-import { useStore, mapState, mapGetters, mapMutations } from 'vuex';
-
+import { useStore, mapState, mapGetters, mapMutations, mapActions } from 'vuex';
 
 export default defineComponent({
   name: "App",
   components: {
-    Button
+    Button,
   },
   setup() {
-    const store = useStore(key);
     const list = ref<string>('');
-
-    const addList = computed(function () {
-      store.commit('additem', {
-        payload: list.value
-      });
-      list.value = '';
-    });
 
     return {
       list,
-      addList,
     }
   },
   methods: {
-    ...mapMutations(['additem']),
+    ...mapActions(['addItem', 'removeItem']),
+    todosAddItem() {
+      console.log(this.$refs?.todosLine);
+      this.addItem(this.list);
+      this.list = '';
+    }
+
   },
   computed: {
     ...mapState({
@@ -37,7 +33,7 @@ export default defineComponent({
       }
     }),
     ...mapGetters(['todosLenght']),
-  }
+  },
 });
 </script>
 
@@ -50,12 +46,14 @@ export default defineComponent({
           <input type="text" class="form-control" v-model="list">
         </div>
         <div class="card-body d-flex justify-content-center pt-2 pb-2">
-          <Button :length="list.length" @addTodoList="() => addList" />
+          <Button :length="list.length" @addTodoList="todosAddItem" />
         </div>
         <div class="card-footer" v-if="todos.length > 0">
           <div class="row">
-            <div class="col-12 border border-0 border-bottom mt-1 mb-1 pt-1 pb-1" v-for="{ item } in todos">
-              <p class="card--text m-0">{{ item }}</p>
+            <div class="col-12 border border-0 border-bottom mt-1 mb-1 pt-1 pb-1" v-for="{ item, id } in todos"
+              ref="todosLine">
+              <p class="card--text m-0 bg-white border-0 rounded-2 p-2 item__line">{{ item }}</p>
+              <button class="btn btn-danger p-2 btn__remove" @click="() => removeItem(id)">Remove</button>
             </div>
           </div>
         </div>
@@ -63,3 +61,12 @@ export default defineComponent({
     </div>
   </div>
 </template>
+
+<style lang="scss">
+.item__line {}
+
+.btn__remove {
+  font-size: 12px !important;
+  margin: 10px 0;
+}
+</style>
