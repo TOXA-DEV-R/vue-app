9185d6f772fa88358c3ecbc41686ff08b4310796
Tohir <TOXIR_DEV>
2022-08-22 21:50:43 +0600

    ended

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,15 +2,24 @@
 
 import { InjectionKey } from "vue";
 import { createStore, Store, ActionPayload } from "vuex";
+import { v4 as uuidv4 } from "uuid";
 
 export interface Item {
   item: string;
+  id: string;
 }
 
 export interface State {
   todos: Item[];
   user: string;
 }
+
+const ActionTypes = {
+  ADD_ITEM: "ADD_ITEM",
+  REMOVE_ITEM: "REMOVE_ITEM",
+};
+
+const { ADD_ITEM, REMOVE_ITEM } = ActionTypes;
 
 const key: InjectionKey<Store<State>> = Symbol();
 
@@ -20,8 +29,20 @@ const store = createStore<State>({
     user: "Husan",
   },
   mutations: {
-    additem(state: State, { payload }: ActionPayload): void {
-      state.todos.push({ item: payload });
+    [ADD_ITEM](state: State, { payload }: ActionPayload): void {
+      state.todos.push({ item: payload, id: uuidv4() });
+    },
+    [REMOVE_ITEM](state, { payload }: ActionPayload) {
+      state.todos = state.todos.filter((item) => item.id !== payload);
+    },
+  },
+  actions: {
+    addItem({ commit }, payload) {
+      console.log(payload);
+      commit(ADD_ITEM, { payload: payload });
+    },
+    removeItem({ commit }, payload) {
+      commit(REMOVE_ITEM, { payload: payload });
     },
   },
   getters: {
